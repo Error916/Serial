13db1d8ff97c7c1ef2558bdb07b291feb083ceb0
Error916 <ettorenigma@gmail.com>
2024-10-15 18:30:25 +0200

    Worked on the Unix part of the library and wrote some boilerplaiting for the windows part

--- src/serial.c
+++ src/serial.c
@@ -1,52 +1,229 @@
 #include "serial.h"
 
-serial_t *serial_new(const char *name, uint8_t quiet) {
-    serial_t *s = malloc(sizeof(serial_t));
+#ifdef __unix__
+
+serial_t* serial_new(const char* name, uint8_t quiet) {
+    serial_t* s = malloc(sizeof(serial_t));
     strncpy(s->name, name, 256);
-    (void) quiet; // ignore quiet param
+    (void)quiet; // ignore quiet param
     s->fd = open(name, O_RDWR | O_NOCTTY | O_NONBLOCK);
     if (s->fd == -1) {
-	    perror("serial_new: Unable to open port - ");
+        perror("serial_new: Unable to open port - ");
         free(s);
         return NULL;
     }
     ssize_t ok = tcgetattr(s->fd, &s->options);
-    if ( ok < 0) {
+    if (ok < 0) {
+        perror("serial_new: Unable to get termios attributes - ");
         close(s->fd);
         free(s);
         return NULL;
     }
 
-    s->_CFLAG = s->options.c_cflag;
-    s->_IFLAG = s->options.c_iflag;
-    s->_ISPEED = s->options.c_ispeed;
-    s->_LFLAG = s->options.c_lflag;
-    s->_OFLAG = s->options.c_oflag;
-    s->_OSPEED = s->options.c_ospeed;
+    // saving the original values
+    s->_cflag = s->options.c_cflag;
+    s->_iflag = s->options.c_iflag;
+    s->_ispeed = s->options.c_ispeed;
+    s->_lflag = s->options.c_lflag;
+    s->_oflag = s->options.c_oflag;
+    s->_ospeed = s->options.c_ospeed;
+    s->_veof = s->options.c_cc[VEOF];
+    s->_veol = s->options.c_cc[VEOL];
+    s->_verase = s->options.c_cc[VERASE];
+    s->_vintr = s->options.c_cc[VINTR];
+    s->_vkill = s->options.c_cc[VKILL];
+    s->_vquit = s->options.c_cc[VQUIT];
+    s->_vsusp = s->options.c_cc[VSUSP];
+    s->_vstart = s->options.c_cc[VSTART];
+    s->_vsto = s->options.c_cc[VSTO];
+    s->_vmin = s->options.c_cc[VMIN];
+    s->_vtime = s->options.c_cc[VTIME];
+
+    // copy the original values into "current" value
+    s->c_cflag = s->_cflag;
+    s->c_iflag = s->_iflag;
+    s->c_ispeed = s->_ispeed;
+    s->c_lflag = s->_lflag;
+    s->c_oflag = s->_oflag;
+    s->c_ospeed = s->_ospeed;
+    s->c_veof = s->_veof;
+    s->c_veol = s->_veol;
+    s->c_verase = s->_verase;
+    s->c_vintr = s->_vintr;
+    s->c_vkill = s->_vkill;
+    s->c_vquit = s->_vquit;
+    s->c_vsusp = s->_vsusp;
+    s->c_vstart = s->_vstart;
+    s->c_vstop = s->_vstop;
+    s->c_vmin = s->_vmin;
+    s->c_vtime = s->_vtime;
+
+    // default to "raw" mode
+    s->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | IGNPAR | INPCK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    s->c_oflag &= ~OPOST;
+    s->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+
+    // sane port
+    s->c_iflag |= IGNBRK;
+    s->c_cflag |= (CLOCAL | CREAD);
+
+    // 9600 baud
+    s->c_ispeed = B9600;
+    s->c_ospeed = B9600;
+
+    // 8data bits
+    s->c_cflag &= ~CSIZE;
+    s->c_cflag |= CS8;
+
+    // disable parity
+    s->c_cflag &= ~(PARENB | PARODD);
+
+    // 1 stop bit
+    s->c_cflag &= ~CSTOPB;
+
+    s->iossiospeed_baud = -1;
+
+    serial_write_settings(s);
+
+    strncpy(s->alias, s->name, 256);
+
+    // private data
+    s->rconst = 0;
+    s->rtot = 0;
+
+    if (can_ioctl(s)) {
+        perror("serial_new: Port not usable to iotcl - ");
+        close(s->fd);
+        free(s);
+        return NULL;
+    }
 
     return s;
 }
 
-ssize_t serial_write(serial_t *s, uint8_t *data, uint64_t datalen) {
+void serial_destroy(serial_t* s) {
+    close(s->fd);
+    free(s);
+}
+
+int serial_write_settings(serial_t* s) {
+    s->options.c_cflag      = s->c_cflag;
+    s->options.c_iflag      = s->c_iflag;
+    s->options.c_ispeed     = s->c_ispeed;
+    s->options.c_lflag      = s->c_lflag;
+    s->options.c_oflag      = s->c_oflag;
+    s->options.c_ospeed     = s->c_ospeed;
+    s->options.c_cc[VEOF]   = s->c_veof;
+    s->options.c_cc[VEOL]   = s->c_veol;
+    s->options.c_cc[VERASE] = s->c_verase;
+    s->options.c_cc[VINTR]  = s->c_vintr;
+    s->options.c_cc[VKILL]  = s->c_vkill;
+    s->options.c_cc[VQUIT]  = s->c_vquit;
+    s->options.c_cc[VSUSP]  = s->c_vsusp;
+    s->options.c_cc[VSTART] = s->c_vstart;
+    s->options.c_cc[VSTO]   = s->c_vsto;
+    s->options.c_cc[VMIN]   = s->c_vmin;
+    s->options.c_cc[VTIME]  = s->c_vtime;
+
+    return tcsetattr(s->fd, TCSANOW, &s->options);
+}
+
+ssize_t serial_write(serial_t* s, uint8_t* data, uint64_t datalen) {
     ssize_t n = write(s->fd, data, datalen);
-    if ( n < datalen )
+    if (n < datalen)
         printf("write of %ld bytes failed\n", datalen);
     return n;
 }
 
-void serial_read_char_time(serial_t *s, uint64_t timems) {}
+ssize_t serial_read(serial_t* s, uint8_t* buf, uint64_t toread) {
+    if (toread == 0) return 0;
+    size_t done = 0;
+    size_t count_in = 0;
+    uint8_t string_in[255] = { 0 };
+    uint8_t *in2 = buf;
+    size_t bufsize = 255; // VMIN max
 
-void serial_read_const_time(serial_t *s, uint64_t timems) {}
-
-ssize_t serial_read(serial_t *s, uint8_t *buf, uint64_t toread, uint8_t noblocking) {
-    if ( noblocking )
-        fcntl(s->fd, F_SETFL, FNDELAY);
-    ssize_t n = read(s->fd, buf, toread);
-    fcntl(s->fd, F_SETFL, 0);
-    return n;
+    while (done < toread) {
+        size_t size = toread - done;
+        if (size > bufsize) size = bufsize;
+        size_t rin = 0;
+        rin |= (1 << s->fd); // vec(rin, fd, 1) = 1 TODO capire meglio
+        struct timeval timer = { 0 };
+        timer.tv_usec= s->RCONST + (toread * s->RTOT);
+        int ready = select(s->fd, &rin, NULL, &rin, &timer);
+        if (ready > 0)
+            count_in = read(s->fd, string_in, size);
+        if (count_in) {
+            strcat(buf, string_in);
+            done += count_in;
+        }
+        else if (done) break;
+        else return 0;
+    }
+    return done;
 }
 
-void serial_destroy(serial_t *s) {
-    close(s->fd);
-    free(s);
+// true/false capabilities (read only)
+int serial_can_baud(serial_t* s)                { (void)s; return 1; }
+int serial_can_databits(serial_t* s)            { (void)s; return 1; }
+int serial_can_stopbits(serial_t* s)            { (void)s; return 1; }
+int serial_can_dtrdsr(serial_t* s)              { (void)s; return 1; }
+int serial_can_handshake(serial_t* s)           { (void)s; return 1; }
+int serial_can_parity_check(serial_t* s)        { (void)s; return 1; }
+int serial_can_parity_config(serial_t* s)       { (void)s; return 1; }
+int serial_can_parity_enable(serial_t* s)       { (void)s; return 1; }
+int serial_can_rlsd(serial_t* s)                { (void)s; return 0; } // currently
+int serial_can_16bitmode(serial_t* s)           { (void)s; return 0; } // Win32 - specific
+int serial_is_rs232(serial_t* s)                { (void)s; return 1; }
+int serial_is_modem(serial_t* s)                { (void)s; return 0; } // Win32 - specific
+int serial_can_rtscts(serial_t* s)              { (void)s; return 1; } // this is a flow option
+int serial_can_xonxoff(serial_t* s)             { (void)s; return 1; } // this is a flow option
+int serial_can_xon_char(serial_t* s)            { (void)s; return 1; } // use stty
+int serial_can_spec_char(serial_t* s)           { (void)s; return 0; } // use stty
+int serial_can_interval_timeout(serial_t* s)    { (void)s; return 0; } // currently
+int serial_can_total_timeout(serial_t* s)       { (void)s; return 1; } // currently
+int serial_binary(serial_t* s)                  { (void)s; return 1; }
+int serial_reset_error(serial_t* s)             { (void)s; return 0; } // for compatibility
+
+int serial_can_ioctl(serial_t *s) {
+    int status;
+    ioctl(s->fd, TIOCMGET, &status);
+    if ((status & TIOCMBIS) && (status & TIOCMBIC) && (status & TIOCM_RTS) &&
+        (((status & TIOCSDTR) && (status & TIOCCDTR)) || (status & TIOCM_DTR)))
+        return 1;
+    return 0;
 }
+
+#endif
+
+#ifdef _WIN32
+serial_t* serial_new(const char* name, uint8_t quiet) { return NULL; }
+void serial_destroy(serial_t* s) {}
+
+int serial_write_settings(serial_t* s) { return 0; }
+
+ssize_t serial_write(serial_t* s, uint8_t* data, uint64_t datalen) { return 0; }
+ssize_t serial_read(serial_t* s, uint8_t* buf, uint64_t toread, uint8_t noblocking) { return 0; }
+
+int serial_can_baud(serial_t* s) { (void)s; return 0; }
+int serial_can_databits(serial_t* s) { (void)s; return 0; }
+int serial_can_stopbits(serial_t* s) { (void)s; return 0; }
+int serial_can_dtrdsr(serial_t* s) { (void)s; return 0; }
+int serial_can_handshake(serial_t* s) { (void)s; return 0; }
+int serial_can_parity_check(serial_t* s) { (void)s; return 0; }
+int serial_can_parity_config(serial_t* s) { (void)s; return 0; }
+int serial_can_parity_enable(serial_t* s) { (void)s; return 0; }
+int serial_can_rlsd(serial_t* s) { (void)s; return 0; }
+int serial_can_16bitmode(serial_t* s) { (void)s; return 0; }
+int serial_is_rs232(serial_t* s) { (void)s; return 0; }
+int serial_is_modem(serial_t* s) { (void)s; return 0; }
+int serial_can_rtscts(serial_t* s) { (void)s; return 0; }
+int serial_can_xonxoff(serial_t* s) { (void)s; return 0; }
+int serial_can_xon_char(serial_t* s) { (void)s; return 0; }
+int serial_can_spec_char(serial_t* s) { (void)s; return 0; }
+int serial_can_interval_timeout(serial_t* s) { (void)s; return 0; }
+int serial_can_total_timeout(serial_t* s) { (void)s; return 0; }
+int serial_binary(serial_t* s) { (void)s; return 0; }
+int serial_reset_error(serial_t* s) { (void)s; return 0; }
+int serial_can_ioctl(serial_t* s) { (void)s; return 0; }
+#endif
--- src/serial.h
+++ src/serial.h
@@ -5,32 +5,100 @@
 #include <stdlib.h>  /* Malloc and free */
 #include <stdint.h>  /* Standard int definitions */
 #include <string.h>  /* String function definitions */
-#include <unistd.h>  /* UNIX standard function definitions */
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
+
+#ifdef __unix__
+#include <unistd.h>  /* UNIX standard function definitions */
 #include <termios.h> /* POSIX terminal control definitions */
 
 typedef struct {
     int fd;
     struct termios options;
-    int _CFLAG;
-    int _IFLAG;
-    int _ISPEED;
-    int _LFLAG;
-    int _OFLAG;
-    int _OSPEED;
+    int _cflag;
+    int _iflag;
+    int _ispeed;
+    int _lflag;
+    int _oflag;
+    int _ospeed;
+    int _veof;
+    int _veol;
+    int _verase;
+    int _vintr;
+    int _vkill;
+    int _vquit;
+    int _vsusp;
+    int _vstart;
+    int _vstop;
+    int _vmin;
+    int _vtime;
+    int c_cflag;
+    int c_iflag;
+    int c_ispeed;
+    int c_lflag;
+    int c_oflag;
+    int c_ospeed;
+    int c_veof;
+    int c_veol;
+    int c_verase;
+    int c_vintr;
+    int c_vkill;
+    int c_vquit;
+    int c_vsusp;
+    int c_vstart;
+    int c_vstop;
+    int c_vmin;
+    int c_vtime;
+    int iossiospeed_baud;
+    int rconst;
+    int rtot;
+    char name[256]; // should be the path lenght limit
+    char alias[256];
+} serial_t;
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+
+typedef struct {
+    HANDLE device;
     char name[256]; // should be the path lenght limit
 } serial_t;
 
+typedef long long int ssize_t;
+#endif
+
 // https://www.msweet.org/serial/serial.html#2_1
 // https://metacpan.org/dist/Device-SerialPort/source/SerialPort.pm
-// https://stackoverflow.com/questions/8666378/detect-windows-or-linux-in-c-c
-ssize_t serial_write(serial_t *s, uint8_t *data, uint64_t datalen);
-void serial_read_char_time(serial_t *s, uint64_t timems);
-void serial_read_const_time(serial_t *s, uint64_t timems);
-ssize_t serial_read(serial_t *s, uint8_t *buf, uint64_t toread, uint8_t noblocking);
+// https://www.pololu.com/docs/0J73/15.6
 
-serial_t *serial_new(const char *name, uint8_t quiet);
+serial_t* serial_new(const char *name, uint8_t quiet);
 void serial_destroy(serial_t *s);
 
+int serial_write_settings(serial_t *s);
+
+ssize_t serial_write(serial_t *s, uint8_t *data, uint64_t datalen);
+ssize_t serial_read(serial_t *s, uint8_t *buf, uint64_t toread, uint8_t noblocking);
+
+int serial_can_baud(serial_t* s);
+int serial_can_databits(serial_t* s);
+int serial_can_stopbits(serial_t* s);
+int serial_can_dtrdsr(serial_t* s);
+int serial_can_handshake(serial_t* s);
+int serial_can_parity_check(serial_t* s);
+int serial_can_parity_config(serial_t* s);
+int serial_can_parity_enable(serial_t* s);
+int serial_can_rlsd(serial_t* s);
+int serial_can_16bitmode(serial_t* s);
+int serial_is_rs232(serial_t* s);
+int serial_is_modem(serial_t* s);
+int serial_can_rtscts(serial_t* s);
+int serial_can_xonxoff(serial_t* s);
+int serial_can_xon_char(serial_t* s);
+int serial_can_spec_char(serial_t* s);
+int serial_can_interval_timeout(serial_t* s);
+int serial_can_total_timeout(serial_t* s);
+int serial_binary(serial_t* s);
+int serial_reset_error(serial_t* s);
+int serial_can_ioctl(serial_t *s);
 #endif
